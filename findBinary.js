5da2c130b1f53e8926b31626e42a42476ae13f78
benessilfie <bquansah007@gmail.com>
2022-12-14 18:01:12 +0000

    Add algorithm:find binary

---
+++ findBinary.js
@@ -0,0 +1,9 @@
+function findBinary(decimal) {
+  let result = "";
+  if (decimal === 0) return result;
+
+  result = (decimal % 2) + result;
+  return findBinary(Math.floor(decimal / 2)) + result;
+}
+
+console.log(findBinary(0));
