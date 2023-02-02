d3740ab26a3fce25387ae459aaf999af1f18628c
benessilfie <bquansah007@gmail.com>
2023-02-02 21:54:26 +0000

    Add problem solving pattern: Sliding window

---
+++ patterns/sliding_window/maxSubarraySum_naive.js
@@ -0,0 +1,22 @@
+function maxSubarraySum(arr, num) {
+  if (num > arr.length) return null;
+
+  let max = -Infinity;
+
+  for (let i = 0; i < arr.length - num + 1; i++) {
+    let temp = 0;
+
+    for (let j = 0; j < num; j++) {
+      temp += arr[i + j];
+    }
+
+    if (temp > max) {
+      max = temp;
+    }
+  }
+
+  return max;
+}
+
+
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)); // 10
